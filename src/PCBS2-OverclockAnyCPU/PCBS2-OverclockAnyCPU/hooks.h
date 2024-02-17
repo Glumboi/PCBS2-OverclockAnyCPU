3ae3fb037ec0910d349046c2734e1e6e31a2b63a
Glumboi <58010838+Glumboi@users.noreply.github.com>
2024-02-17 21:38:00 +0100

    Updated to overwrite full game functions

--- src/PCBS2-OverclockAnyCPU/PCBS2-OverclockAnyCPU/hooks.h
+++ src/PCBS2-OverclockAnyCPU/PCBS2-OverclockAnyCPU/hooks.h
@@ -1,25 +1,66 @@
 #pragma once
-#include <iostream>
-#include "offsets.h"
+
+//Can be used to alter unity behavior
+
 #define MODNAME "[PCBS2-OverclockAnyCPU]"
 #define JSONFILE "\\Plugins\\PCBS2-OverclockAnyCPU.offsetRequests.json"
 #define QUICKDEBUG(msg) std::cout << MODNAME << ": " <<  msg << "\n"
 
 //Define your hooks here, or somewhere else and include them here
-//Example from my infinite money plugin:
-/*
-int32_t(__fastcall* CareerStatus__GetCash_o)(DWORD*, DWORD*);
+void (__fastcall*PartDescCPU_GetHWInfoProps_o)(DWORD*, DWORD*, const DWORD*);
 
-int32_t __stdcall CareerStatus__GetCash_hook(DWORD* __tis, DWORD* method)
+void __stdcall PartDescCPU_GetHWInfoProps_hook(DWORD* __this, DWORD* props, const DWORD* method)
 {
-    return 1000000;
-}
-*/
+    //init standard values
+    PartDescCPU_GetHWInfoProps_o(__this, props, method);
 
-bool(__fastcall* BiosConfig_AllowOverclock_o)(DWORD*, const DWORD*);
+    Unity::CComponent* caller = (Unity::CComponent*)__this;
+
+    std::vector<Unity::il2cppFieldInfo*> fields;
+
+    caller->SetMemberValue<bool>("m_canOverclock", true);
+    //caller->SetMemberValue<bool>("m_canDelid", true);
+    caller->SetMemberValue<int>("m_maxFreqMhz", 20000);
+
+    float currentStepMultipler = caller->GetMemberValue<float>("m_multiplierStep");
+    if (currentStepMultipler == 0)
+    {
+        caller->SetMemberValue<float>("m_multiplierStep", .25f);
+    }
+
+    return;
+}
+
+bool (__fastcall*BiosConfig_AllowOverclock_o)(DWORD*, const DWORD*);
 
 bool __stdcall BiosConfig_AllowOverclock_hook(DWORD* __this, const DWORD* method)
 {
+    bool returnResult = BiosConfig_AllowOverclock_o(__this, method);
+    if (returnResult)
+        return returnResult;
+    return true;
+}
+
+
+bool (__fastcall*BiosConfig_ChangeCPUMultiplier_o)(DWORD*, int32_t, const DWORD*);
+
+bool __stdcall BiosConfig_ChangeCPUMultiplier_hook(DWORD* __this, int32_t dir, const DWORD* method)
+{
+    Unity::CComponent* caller = (Unity::CComponent*)__this;
+    float cpuMultiplier = caller->GetMemberValue<float>("m_cpuMultiplier");
+    QUICKDEBUG("cpu multiplier: " << cpuMultiplier << " dir: " << dir);
+
+    if (dir == 1) // Incresase
+    {
+        cpuMultiplier += 0.25f;
+    }
+    else if (dir == -1) // Decrease
+    {
+        cpuMultiplier -= 0.25f;
+    }
+
+    caller->SetMemberValue<float>("m_cpuMultiplier", cpuMultiplier);
+
     return true;
 }
 
@@ -35,12 +76,24 @@ inline void CreateAndLoadHooks()
     //Load offsets
     functionOffsets = readJsonFromFile(dllJsonPath).offsetRequests;
 
-    //Instantiate your hooks here using MH_CreateHook
-    uintptr_t BiosConfig_AllowOverclockOffset = std::stoull(functionOffsets[0].value, nullptr, 16);
+    uintptr_t PartDescCPU_GetHWInfoPropsOffset = std::stoull(functionOffsets[0].value, nullptr, 16);
+    MH_CreateHook(
+        reinterpret_cast<LPVOID*>(gameAsm + PartDescCPU_GetHWInfoPropsOffset),
+        &PartDescCPU_GetHWInfoProps_hook,
+        (LPVOID*)&PartDescCPU_GetHWInfoProps_o);
+
+
+    uintptr_t BiosConfig_AllowOverclockOffset = std::stoull(functionOffsets[1].value, nullptr, 16);
     MH_CreateHook(
         reinterpret_cast<LPVOID*>(gameAsm + BiosConfig_AllowOverclockOffset),
         &BiosConfig_AllowOverclock_hook,
         (LPVOID*)&BiosConfig_AllowOverclock_o);
+
+    uintptr_t BiosConfig_ChangeCPUMultiplier_hookOffset = std::stoull(functionOffsets[2].value, nullptr, 16);
+    MH_CreateHook(
+        reinterpret_cast<LPVOID*>(gameAsm + BiosConfig_ChangeCPUMultiplier_hookOffset),
+        &BiosConfig_ChangeCPUMultiplier_hook,
+        (LPVOID*)&BiosConfig_ChangeCPUMultiplier_o);
 
 
     MH_STATUS status = MH_EnableHook(MH_ALL_HOOKS); //Get the hook status, 0 = ALl good
---
+++ src/PCBS2-OverclockAnyCPU/PCBS2-OverclockAnyCPU/includes.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <MinHook.h>
+#include <sstream>
+
+#include "./IL2CppResolver/IL2CPP_Resolver.hpp"
+
+#include "offsets.h"
+#include "hooks.h"
+#include "debugUtils.h"
